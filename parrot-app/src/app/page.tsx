357df5c1769ce9b7a7a0a24d204e04bc1728b865
anishagrawal <anishagrawal2003@gmail.com>
2024-07-07 01:40:04 -0400

    custom tabs

--- parrot-app/src/app/page.tsx
+++ parrot-app/src/app/page.tsx
@@ -1,28 +1,27 @@
 import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
 } from "@/components/ui/resizable"
+import TabSidebar from "@/components/tab-sidebar/tab-sidebar";
 
 export default function Home() {
-  return (
-      <div className="w-screen h-screen">
-        <ResizablePanelGroup
-            direction="horizontal"
-            className="rounded-lg border"
-        >
-          <ResizablePanel defaultSize={25}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Sidebar</span>
-            </div>
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={75}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Content</span>
-            </div>
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </div>
-  );
+    return (
+        <div className="w-screen h-screen">
+            <ResizablePanelGroup
+                direction="horizontal"
+                className="rounded-lg border"
+            >
+                <ResizablePanel defaultSize={25}>
+                    <div className="flex p-6">
+                        <span className="font-semibold">Sidebar</span>
+                    </div>
+                </ResizablePanel>
+                <ResizableHandle withHandle/>
+                <ResizablePanel defaultSize={75}>
+                    <TabSidebar/>
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </div>
+    );
 }
