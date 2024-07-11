9b735f3879cab2f69d4311bfe80e87eb01dc5ed5
anishagrawal <anishagrawal2003@gmail.com>
2024-07-11 03:58:56 -0400

    CORS

--- parrot-api/main.py
+++ parrot-api/main.py
@@ -11,8 +11,17 @@ from starlette.responses import StreamingResponse
 
 from models.request_models import AgentKickoffRequest
 from use_cases.agent_use_case import DAgentUseCase
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -23,17 +32,11 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/test")
-def test():
-
-    return {"item_id": "asst_id"}
-
-
 @app.post("/query")
 async def run(request: AgentKickoffRequest, agent: DAgentUseCase):
 
     async def event_stream():
         async for json_log in agent.kickoff(params=request):
-            yield json_log + '\n'
+            yield f"data: {json_log}\n\n"
 
-    return StreamingResponse(event_stream(), media_type="application/x-ndjson")
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
